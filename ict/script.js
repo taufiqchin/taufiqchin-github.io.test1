49ce226dac13829162fd86338a7904772da20208
taufiqchin <taufiq0202@gmail.com>
2023-11-30 08:18:54 +0800

    add maps to ict

--- ict/script.js
+++ ict/script.js
@@ -16,3 +16,33 @@ window.onload = function() {
   var imageHeight = containerHeight * 0.6; // Set the image height to 60% of the container's height
   image.style.height = imageHeight + 'px';
 };
+
+
+// button copy link
+    function copyToClipboard() {
+      // Get the text content of the link
+      var linkText = document.getElementById("map-link").innerText;
+
+      // Create a temporary input element
+      var tempInput = document.createElement("input");
+      tempInput.setAttribute("value", linkText);
+
+      // Append the input element to the document
+      document.body.appendChild(tempInput);
+
+      // Select the text content of the input element
+      tempInput.select();
+      tempInput.setSelectionRange(0, 99999); // For mobile devices
+
+      // Copy the selected text to the clipboard
+      document.execCommand("copy");
+
+      // Remove the temporary input element
+      document.body.removeChild(tempInput);
+
+      // Provide feedback (you can customize this)
+      //alert("Link copied to clipboard: " + linkText);
+    }
+
+
+
